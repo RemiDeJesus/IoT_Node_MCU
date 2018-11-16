a53bca26045b2e4c5629486821fa21a546b9601e
RemiDeJesus <de.jesus.remi@gmail.com>
2018-11-16 11:20:03 +0100

    controle led et lecture valeurs capteurs

--- mcu/mcu.ino
+++ mcu/mcu.ino
@@ -1,9 +1,80 @@
-void setup() {
-  // put your setup code here, to run once:
+//https://www.instructables.com/id/Quick-Start-to-Nodemcu-ESP8266-on-Arduino-IDE/
+//https://www.adafruit.com/product/385
+//https://www.carnetdumaker.net/articles/utiliser-un-capteur-de-temperature-et-dhumidite-dht11-dht22-avec-une-carte-arduino-genuino/
 
+#include "DHTesp.h"
+
+#ifdef ESP32
+#pragma message(THIS EXAMPLE IS FOR ESP8266 ONLY!)
+#error Select ESP8266 board.
+#endif
+
+DHTesp dht;
+
+#define ON 1
+#define OFF 0
+
+void setup() {
+
+  //Setup PIN
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+
+  //Setup DHT22
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)\tHeatIndex (C)\t(F)");
+  String thisBoard= ARDUINO_BOARD;
+  Serial.println(thisBoard);
+
+  // Autodetect is not working reliable, don't use the following line
+  // dht.setup(17);
+  // use this instead: 
+  dht.setup(12, DHTesp::DHT22); // Connect DHT sensor to GPIO 17
 }
+
+void set_led(int val){
+  if (val){
+    digitalWrite(LED_BUILTIN, LOW);
+  }else{
+     digitalWrite(LED_BUILTIN, HIGH);
+  }
+  return;
+  
+}
+
+
+void DHT()
+{
+  delay(dht.getMinimumSamplingPeriod());
+
+  float humidity = dht.getHumidity();
+  float temperature = dht.getTemperature();
+
+  Serial.print(dht.getStatusString());
+  Serial.print("\t");
+  Serial.print(humidity, 1);
+  Serial.print("\t\t");
+  Serial.print(temperature, 1);
+  Serial.print("\t\t");
+  Serial.print(dht.toFahrenheit(temperature), 1);
+  Serial.print("\t\t");
+  Serial.print(dht.computeHeatIndex(temperature, humidity, false), 1);
+  Serial.print("\t\t");
+  Serial.println(dht.computeHeatIndex(dht.toFahrenheit(temperature), humidity, true), 1);
+}
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  set_led(ON);
+  DHT();
+  /*
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on by making the voltage LOW
+  delay(1000);                      // Wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  delay(2000);  
+  // Wait for two seconds
+  */
 }
+
+
